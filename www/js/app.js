3a3a6a41008fb0d1938d29c6f105b63506359b66
jmartinez-ebcomm <jmartinez@ebcomm.mx>
2017-01-24 19:46:27 -0600

    Actualizacion

--- www/js/app.js
+++ www/js/app.js
@@ -40,20 +40,18 @@ var app = {
 
         // members
         var parameters = this.getParameters();
-	/*
-        this.browserUrlCurrent = decodeURIComponent(parameters['current']);
-        this.browserUrlRequest = decodeURIComponent(parameters['request']);
+        //this.browserUrlCurrent = decodeURIComponent(parameters['current']);
+        //this.browserUrlRequest = decodeURIComponent(parameters['request']);
         this.externalRequest = decodeURIComponent(parameters['external']);
-	*/
+
         this.browserUrlCurrent = parameters['current'];
         this.browserUrlRequest = parameters['request'];
-        this.externalRequest = parameters['external'];
 
 	// jmartinez: twitter ^ = %5E
-	if (this.browserUrlCurrent !== undefined && this.browserUrlCurrent != null && this.browserUrlCurrent != '')
+	if (this.browserUrlCurrent !== undefined && this.browserUrlCurrent != '')
 		this.browserUrlCurrent = this.browserUrlCurrent.replace('^', '%5E');
 
-	if (this.browserUrlRequest !== undefined && this.browserUrlRequest != null && this.browserUrlRequest != '')
+	if (this.browserUrlRequest !== undefined && this.browserUrlRequest != '')
 		this.browserUrlRequest = this.browserUrlRequest.replace('^', '%5E');
 
         if (this.externalRequest == 'undefined' || this.externalRequest == '' || this.externalRequest == 'false') {
@@ -92,7 +90,6 @@ var app = {
 
         // set title again
         document.title = app.appTitle;
-	///////////////
 
 	loadingComponent.setAttribute('class', 'loader');
 	statusBoxComponent.setAttribute('class', 'blink');
